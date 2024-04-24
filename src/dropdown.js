4471dd6fdf8d9dc517ab674bfb4fb580014e7fb1
elishamutang <elishamutang@outlook.com>
2024-04-25 00:18:40 +0800

    new build.

---
+++ src/dropdown.js
@@ -0,0 +1,26 @@
+import './styles.css';
+
+export default function dropdownMenu() {
+
+    const dropdown = document.querySelector('.dropdown');
+    const dropdownContent = document.querySelector('.dropdown-content');
+
+    dropdown.addEventListener('click', function() {
+
+        if(![...dropdownContent.classList].includes('open-dropdown')) {
+
+            dropdownContent.className += ' open-dropdown';
+
+        } else {
+            
+            const defaultClass = [...dropdownContent.classList].filter((classes) => {
+                return classes !== 'open-dropdown';
+            })
+
+            dropdownContent.className = defaultClass.join(' ');
+
+        }
+
+    })
+
+}
--- src/index.js
+++ src/index.js
@@ -1,28 +1,3 @@
-import './styles.css';
+import dropdownMenu from "./dropdown";
 
-function dropdownMenu() {
-
-    const dropdown = document.querySelector('.dropdown');
-    const dropdownContent = document.querySelector('.dropdown-content');
-
-    dropdown.addEventListener('click', function() {
-
-        if(![...dropdownContent.classList].includes('open-dropdown')) {
-
-            dropdownContent.className += ' open-dropdown';
-
-        } else {
-            
-            const defaultClass = [...dropdownContent.classList].filter((classes) => {
-                return classes !== 'open-dropdown';
-            })
-
-            dropdownContent.className = defaultClass.join(' ');
-
-        }
-
-    })
-
-}
-
-module.exports = dropdownMenu;
+dropdownMenu();
